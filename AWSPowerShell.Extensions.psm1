515970c87700cecb575e2f32e88c70002c72dfe8
Trevor Sullivan <tsulli@amazon.com>
2018-02-05 06:54:42 -0800

    Add code to import TypeData

--- AWSPowerShell.Extensions.psm1
+++ AWSPowerShell.Extensions.psm1
@@ -1,6 +1,18 @@
 $ModuleRoot = $PSScriptRoot
 
+#region Import Intellisense
 $ScriptList = Get-ChildItem -Path ('{0}/Intellisense' -f $ModuleRoot) -File -Filter *.ps1
 foreach ($Script in $ScriptList) {
     & $Script.FullName
 }
+#endregion
+
+#region Import TypeData
+$TypeAdapterList = Get-ChildItem -Path ('{0}/TypeAdapter' -f $ModuleRoot) -File -Filter *.ps1
+
+foreach($Script in $TypeAdapterList) {
+    foreach ($TypeAdapter in (& $Script.FullName)) {
+        Update-TypeData @TypeAdapter
+    }
+}
+#endregion
