96925c3542163ec572900e8e4d31926210322834
Trevor Sullivan <tsulli@amazon.com>
2018-02-19 21:21:55 -0800

    Add Intellisense for EC2 LaunchTemplateName and LaunchTemplateId parameters

---
+++ Intellisense/AWS.EC2.LaunchTemplateName.completer.ps1
@@ -0,0 +1,26 @@
+<#
+Copyright 2018-2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the License. A copy of the License is located at
+
+    http://aws.amazon.com/apache2.0/
+
+or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+#>
+
+$ScriptBlock = {
+  param($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameter)
+
+  $TemplateList = Get-EC2Template | Where-Object -FilterScript {
+    $PSItem.LaunchTemplateId -match $wordToComplete -or
+    $PSItem.LaunchTemplateName -match $wordToComplete
+  }
+  $TemplateList.LaunchTemplateName
+}
+
+$Completer = @{
+  CommandName = (Get-Command -Module AWSPowerShell* -ParameterName LaunchTemplateName).Name
+  ParameterName = 'LaunchTemplateName'
+  ScriptBlock = $ScriptBlock
+}
+Register-ArgumentCompleter @Completer
